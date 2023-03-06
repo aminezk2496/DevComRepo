cdb90cb0ebc9c70cb7df00cd7047b795fe23e5ce
rihemRD <125923915+rihemRD@users.noreply.github.com>
2023-03-07 00:42:56 +0100

    Add files via upload

---
+++ src/Entities/Camping.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entities;
+import java.awt.Image;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+
+/**
+ *
+ * @author rihem
+ */
+public class Camping {
+    
+    private int Periode,Nbr_PlaceC;
+    private int id_Camping;
+    private String Nom,Lieux,Description;
+    LocalDate date_Debut,date_Fin;
+    private double Prix;
+    private Image image;
+    private String imageC;
+//    id_Camping, Nom, date_Debut, date_Fin, Periode, Prix, Lieux, Description
+    public Camping(int id_Camping,String Nom,LocalDate date_Debut,LocalDate date_Fin,int Periode,double Prix,String Lieux,String Description,Image image,int Nbr_PlaceC,String imageC){
+        this.id_Camping = id_Camping;
+        this.Nom=Nom;
+        this.date_Debut=date_Debut;
+        this.date_Fin = date_Fin;
+        this.Periode = Periode;
+        this.Prix=Prix;
+        this.Lieux=Lieux;
+        this.Description =Description;
+        this.Nbr_PlaceC=Nbr_PlaceC;
+        this.image =image;
+        this.imageC= imageC;
+    }
+ boolean check;
+    
+
+   public Camping() {
+    }  
+   
+public boolean isCheck() {
+            return check;
+        }
+
+    public int getId_Camping() {
+        return id_Camping;
+    }
+    
+    public String getNom() {
+        return Nom;
+    }
+    
+    public LocalDate getDate_Debut() {
+        return date_Debut;
+    }
+
+    public LocalDate getDate_Fin() {
+        return date_Fin;
+    }
+
+    public int getPeriode() {
+        return Periode;
+    }
+
+    public double getPrix(){
+        return Prix;
+    }
+    public int getNbr_PlaceC() {
+        return Nbr_PlaceC;
+    }
+    
+    public Image getImage(){
+        return image;
+    }
+    
+    public String getLieux(){
+        return Lieux;
+    }
+    
+    public String getDescription(){
+        return Description;
+    }
+    public String getImageC(){
+        return imageC ;
+    }
+
+    public void setId_Camping(int id_Camping) {
+        this.id_Camping = id_Camping;
+    }
+    
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+    
+    public void setDate_Debut(LocalDate date_Debut) {
+        this.date_Debut = date_Debut;
+    }
+
+    public void setDate_Fin(LocalDate date_Fin) {
+        this.date_Fin = date_Fin;
+    }
+
+    public void setPeriode(int Periode){
+        this.Periode=Periode;
+    }
+
+    public void setPrix(double Prix){
+        this.Prix=Prix;
+    }
+    
+    public void setNbr_PlaceC(int Nbr_PlaceC){
+        this.Nbr_PlaceC=Nbr_PlaceC;
+    }
+    
+    public void setImage (Image image){
+        this.image=image;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public void setLieux(String Lieux){
+        this.Lieux = Lieux;
+    }
+    public void setImageC (String imageC){
+        this.imageC=imageC;
+    }
+
+    @Override
+    public String toString() {
+        return "Camping { " + " Id Camping = " + id_Camping + ",  Nom = " + Nom +", Date Debut = " + date_Debut +", Date Fin = " + date_Fin +
+                ", Periode = " + Periode + ", Prix ="+ Prix + ", Lieux = "+ Lieux + ", Description = " +Description + '}';
+    }
+        
+}
--- src/Entities/Participation.java
+++ src/Entities/Participation.java
@@ -4,88 +4,108 @@
  */
 package Entities;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 /**
  *
- * @author Amal
+ * @author rihem
  */
 public class Participation {
-    private int id_Parti;
-    private int id_Camp;
-    private int id_Rand;
-    private int Nombre;
-    private int Montant;
-    private String Etat;
-    private String date_Parti;
-    private int  id_evenement;
-    private int  id_utilisateur;
-    public Participation() {
-        //super();
-        super();
-       
-    }
-    public Participation(Integer id_Parti,Integer id_Camp,Integer id_Rand,Integer Nombre,Integer Montant,String Etat,String date_Parti,Integer id_evenement,Integer id_utilisateur) {
-        this.id_Parti = id_Parti;
-        this.id_Camp = id_Camp;
-        this.Nombre= Nombre;
-        this.Montant= Montant;
-        this.Etat= Etat;
-        this.date_Parti= date_Parti;
-        this.id_evenement= id_evenement;
-        this.id_utilisateur= id_utilisateur;
-    }
+    //id_Parti, id_Camp, id_Rand, Nombre, Montant, Etat ,date_Parti
     
-    public Integer getIdEvent() {
+    private String id_Parti, id_Camp, id_Rand ;
+    private int Nombre;
+    private Double Montant;
+    private LocalDate date_Parti;
+    private String Etat,RefP,Nom;
+    public Participation(){
+    }
+    public Participation(String id_Parti,String id_Camp,String id_Rand,int Nombre,Double Montant,String Etat,String RefP ,LocalDate date_Parti,String Nom){
+        this.id_Parti = id_Parti;
+        this.id_Camp=id_Camp;
+        this.id_Rand=id_Rand;
+        this.Nombre=Nombre;
+        this.Montant=Montant;
+        this.Etat=Etat;
+        this.date_Parti=date_Parti;
+        this.RefP=RefP;
+        this.Nom=Nom;
+    }
+
+    public String getNom(){
+        return Nom;
+    }
+    public String getRefP(){
+        return RefP;
+    }
+    public String getId_Parti() {
         return id_Parti;
     }
-    public void setIdParti(Integer id_Parti) {
-        this.id_Parti = id_Parti;
-    }
-    public Integer getIdCamp() {
+    
+    public String getId_Camp() {
         return id_Camp;
     }
-    public void setIdCamp(Integer id_Camp) {
-        this.id_Camp = id_Camp;
-    }
-    public Integer getIdRand() {
+    
+    public String getId_Rand() {
         return id_Rand;
     }
-    public void setIdRand(Integer id_Rand) {
-        this.id_Rand = id_Rand;
-    }
-    public Integer getNombre() {
+    
+    public int getNombre(){
         return Nombre;
     }
-    public void setNombre(Integer Nombre) {
-        this.Nombre = Nombre;
-    }
-    public Integer getMontant() {
+    
+    public double getMontant(){
         return Montant;
     }
-    public void setMontant(Integer Montant) {
-        this.Montant = Montant;
-    }
-    public String getEtat() {
-        return Etat;
-    }
-    public void setEtat(String Etat) {
-        this.Etat = Etat;
-    }
-    public String getDateParti() {
+    
+    public LocalDate getDate_Parti() {
         return date_Parti;
     }
-    public void setDateParti(String date_Parti) {
+    
+    public String getEtat(){
+        return Etat;
+    }
+
+    public void setId_Parti(String id_Parti) {
+        this.id_Parti = id_Parti;
+    }
+    
+     public void setId_Camp(String id_Camp) {
+        this.id_Camp = id_Camp;
+    }
+     
+      public void setId_Rand(String id_Rand) {
+        this.id_Rand = id_Rand;
+    }
+      
+    public void setNombre(int Nombre) {
+        this.Nombre = Nombre;
+    }
+      
+    public void setMontant(Double Montant) {
+        this.Montant = Montant;
+    }
+
+    public void setDate_Parti(LocalDate date_Parti) {
         this.date_Parti = date_Parti;
     }
-    public Integer getIdEvenement() {
-        return id_evenement;
+
+    public void setEtat(String Etat){
+        this.Etat=Etat;
     }
-    public void setIdEvenement(Integer id_evenement) {
-        this.id_evenement = id_evenement;
+    public void setRefP(String RefP){
+        this.RefP = RefP;
     }
-    public Integer getIdUtilisateurt() {
-        return id_utilisateur;
+    public void setNom(String Nom){
+        this.Nom=Nom;
     }
-    public void setIdUtilisateurt(Integer id_utilisateur) {
-        this.id_utilisateur = id_utilisateur;
+    //id_Parti, id_Camp, id_Rand, Nombre, Montant, Etat ,date_Parti
+
+    @Override
+    public String toString() {
+        return "Camping { " + " Id Participation = " + id_Parti + " Id Camping = " + id_Camp + " Id Randonnee = " + id_Rand + "Nombre" + Nombre + 
+                ",  Montant = " + Montant + ", Etat = " + Etat + ", date_Parti ="+ date_Parti +'}';
     }
+    
 }
